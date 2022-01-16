17a3e9faf90ae8ed4bfa93cb62c39bf85c68a846
Andrzej Szelka <szelkaandrzej@gmail.com>
2022-01-16 23:31:16 +0100

    Add migration - add columns nightly prices

---
+++ database/migrations/2022_01_16_232934_add_columns_nightly_price_for_additional_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsNightlyPriceForAdditionalServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('additional_services', function (Blueprint $table) {
+            $table->after('gross_price', function ($table) {
+                $table->decimal('nightly_net_price', 10, 2)->default(0);
+                $table->decimal('nightly_gross_price', 10, 2)->default(0);
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('additional_services', function (Blueprint $table) {
+            $table->dropColumn('nightly_net_price');
+            $table->dropColumn('nightly_gross_price');
+        });
+    }
+}
